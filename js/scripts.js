b8fcbc4ebfd650c50f2e85b35b49e03c17150807
mninosua <157538398+mninosua@users.noreply.github.com>
2024-04-23 21:54:06 -0400

    Updates

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+	// Travel carousel
+		let travelCurrentIndex = 0;  // Separate currentIndex for travel carousel
+        const picsWrapper = document.querySelector('.not-research-travel-pics');
+        const pics = document.querySelectorAll('.travel-pic');
+		  const prevTravelButton = document.getElementById('prev-travel-btn');
+		  const nextTravelButton = document.getElementById('next-travel-btn');
+        const totalPics = pics.length;
+
+        // Function to go to a specific index
+        function goToIndex(index) {
+            if (index < 0) {
+            travelCurrentIndex = totalPics - 1;
+            } else if (index > totalPics - 1) {
+                travelCurrentIndex = 0; //
+            } else {
+                travelCurrentIndex = index;
+            }
+            const offset = travelCurrentIndex * -100;
+            picsWrapper.style.transform = `translateX(${offset}%)`;
+        }
+
+        prevTravelButton.addEventListener('click', function () {
+            goToIndex(travelCurrentIndex - 1);
+        });
+
+        nextTravelButton.addEventListener('click', function () {
+            goToIndex(travelCurrentIndex + 1);
+        });
+
+});
