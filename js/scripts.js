09edf813cd7bad083a939efc5fd418ddc0e49965
mninosua <157538398+mninosua@users.noreply.github.com>
2024-04-30 00:16:39 -0400

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+//Took out the DOMContentLoaded line
 
 	// Travel carousel
 		let travelCurrentIndex = 0;  // Separate currentIndex for travel carousel
@@ -29,4 +29,4 @@ document.addEventListener("DOMContentLoaded", function () {
             goToIndex(travelCurrentIndex + 1);
         });
 
-});
+
